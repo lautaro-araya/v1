ba069a01a7a50069463ab09610107a05724db939
lautaro araya <lautaro.araya69@gmail.com>
2024-12-08 15:46:47 -0300

    ahora si

--- trans_moraga/core/admin.py
+++ trans_moraga/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Camiones, Propietario, Encuestador, FormularioInspeccion, Componente
 # Register your models here.
+admin.site.register(Camiones)
+admin.site.register(Propietario)
+admin.site.register(Encuestador)
+admin.site.register(FormularioInspeccion)
+admin.site.register(Componente)
