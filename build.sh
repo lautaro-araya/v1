988f1f4c87834c5777b0bef9d27feeccec73d199
lautaro araya <lautaro.araya69@gmail.com>
2024-12-15 16:14:00 -0300

    migrations

--- build.sh
+++ build.sh
@@ -5,8 +5,9 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+python manage.py migrate
+python manage.py makemigrations core
 # Convert static asset files
 python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
-python manage.py migrate
