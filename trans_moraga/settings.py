2dcf5c045ba55d620d532a4ff82efb886b924d39
lautaro araya <lautaro.araya69@gmail.com>
2024-12-15 16:03:42 -0300

    allowed host1

--- trans_moraga/settings.py
+++ trans_moraga/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-(e)l@^)rpzm*%b3n4!o4k6_m%)tr91#af10yv+kmv!drss9k@2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['transmoraga.onrender.com']
 
 
 # Application definition
